263e6c0ed2e726038d97c19e0cfc3b72bf95a5d1
Fabio Carvalho <farvalho@outlook.com>
2022-09-09 16:01:18 +0100

    +Add: Default project with app icon

---
+++ InterfaceWonderland/InterfaceWonderland/InterfaceWonderlandApp.swift
@@ -0,0 +1,17 @@
+//
+//  InterfaceWonderlandApp.swift
+//  InterfaceWonderland
+//
+//  Created by Fábio Carvalho on 09/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct InterfaceWonderlandApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ InterfaceWonderland/InterfaceWonderland/Presentation/Views/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  InterfaceWonderland
+//
+//  Created by Fábio Carvalho on 09/09/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
